6e9adcaa671c7e088599cf41ecab319eb5efab4e
gasplas <lasornegaspard@gmail.com>
2021-03-02 13:19:36 +0100

    Change cursor when button is disabled

--- components/Button.jsx
+++ components/Button.jsx
@@ -19,7 +19,7 @@ export const Button = ({
 			className={cn(
 				"flex items-center justify-center rounded focus:outline-none px-3 h-8 border border-accent-2 bg-accent-1 space-x-2",
 				{
-					"text-accent-4": disabled,
+					"text-accent-4 cursor-not-allowed": disabled,
 					"hover:border-accent-3 focus:border-accent-3": !disabled,
 				},
 				className

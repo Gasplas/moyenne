99160ea32d491d21d8a3f7d437981d16b2690952
gasplas <lasornegaspard@gmail.com>
2021-03-03 17:44:48 +0100

    Add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,41 @@ class MyDocument extends Document {
 						type="font/woff2"
 						crossOrigin="anonymous"
 					/>
-					<link rel="icon" href="/favicon.ico" />
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/icons/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/icons/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/icons/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/icons/site.webmanifest" />
+					<link
+						rel="mask-icon"
+						href="/icons/safari-pinned-tab.svg"
+						color="#0095d4"
+					/>
+					<link rel="shortcut icon" href="/icons/favicon.ico" />
+					<meta
+						name="apple-mobile-web-app-title"
+						content="EcoleDirecte"
+					/>
+					<meta name="application-name" content="EcoleDirecte" />
+					<meta name="msapplication-TileColor" content="#ffffff" />
+					<meta
+						name="msapplication-config"
+						content="/icons/browserconfig.xml"
+					/>
+					<meta name="theme-color" content="#ffffff" />
 					<meta name="description" content="Moyenne EcoleDirecte" />
 				</Head>
 				<body className="bg-background text-foreground mx-auto w-full h-full">
